26b242a05c950c6d91b0da52bb2a940fc115bacb
Dimitar Danailov <dimityr.danailov@gmail.com>
2025-06-25 08:33:50 +0300

    Cities are exported in a different file

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,107 +2,25 @@ import { expect } from "chai";
 import { calculateBoundingCoordinates } from "../src";
 import GeoLocation from "../src/GeoLocation";
 
-type City = {
-  name: string;
-  lat: number;
-  lng: number;
-};
-
-const London: City = {
-  name: "London",
-  lat: 51.5074,
-  lng: -0.1278,
-};
-
-const Brighton: City = {
-  name: "Brighton",
-  lat: 50.8225,
-  lng: -0.1372,
-};
-
-const Manchester: City = {
-  name: "Manchester",
-  lat: 53.483959,
-  lng: -2.244644,
-};
-
-const Guildford: City = {
-  name: "Guildford",
-  lat: 51.2362,
-  lng: -0.5704,
-};
-
-const Edinburgh: City = {
-  name: "Edinburgh",
-  lat: 55.9533,
-  lng: -3.1883,
-};
-
-export const NewYork: City = {
-  name: "New York",
-  lat: 40.7128,
-  lng: -74.006,
-};
-
-const LosAngeles: City = {
-  name: "Los Angeles",
-  lat: 34.0522,
-  lng: -118.2437,
-};
-
-const SanFrancisco: City = {
-  name: "San Francisco",
-  lat: 37.7749,
-  lng: -122.4194,
-};
-
-const BuenosAres: City = {
-  name: "Buenos Aires",
-  lat: -34.6037,
-  lng: -58.3816,
-};
-
-const Cordoba: City = {
-  name: "Córdoba",
-  lat: -31.4201,
-  lng: -64.1888,
-};
-
-const Sydney: City = {
-  name: "Sydney",
-  lat: -33.8688,
-  lng: 151.2093,
-};
-
-const Greenwich: City = {
-  name: "Greenwich",
-  lat: 51.4769,
-  lng: 0.0005,
-};
-
-const Equator: City = {
-  name: "Ecuator",
-  lat: 0,
-  lng: 0,
-};
-
-const Krakow: City = {
-  name: "Krakow",
-  lat: 50.0647,
-  lng: 19.945,
-};
-
-const NorthPole: City = {
-  name: "North Pole",
-  lat: 90,
-  lng: 0,
-};
-
-const SouthPole: City = {
-  name: "South Pole",
-  lat: -90,
-  lng: 0,
-};
+import {
+  City,
+  SanFrancisco,
+  Greenwich,
+  London,
+  Brighton,
+  Guildford,
+  Manchester,
+  Edinburgh,
+  Sydney,
+  NewYork,
+  LosAngeles,
+  Krakow,
+  BuenosAres,
+  Cordoba,
+  NorthPole,
+  Equator,
+  SouthPole,
+} from "../src/cities";
 
 function isBoundingCoordinates(
   cityA: City,
